17d66bb30d5bf99b60f851440cdccbe0b4d308b3
sitebytom <20994642+sitebytom@users.noreply.github.com>
2026-01-29 15:29:02 +0000

    chore: release v1.0.2

--- dev/src/app/(payload)/admin/importMap.js
+++ dev/src/app/(payload)/admin/importMap.js
@@ -1,13 +1,7 @@
-import { FolderTableCell as FolderTableCell_ab83ff7e88da8d3530831f296ec4756a } from '@payloadcms/ui/rsc'
-import { FolderField as FolderField_ab83ff7e88da8d3530831f296ec4756a } from '@payloadcms/ui/rsc'
-import { MediaListView as MediaListView_642efd537a03a6c81446512d5792dfd8 } from 'payload-media-gallery/client'
-import { FolderTypeField as FolderTypeField_3817bf644402e67bfe6577f60ef982de } from '@payloadcms/ui'
+import { MediaListView as MediaListView_6f514b404e7f1589cac282e4c156f993 } from '@sitebytom/payload-media-gallery/client'
 import { CollectionCards as CollectionCards_ab83ff7e88da8d3530831f296ec4756a } from '@payloadcms/ui/rsc'
 
 export const importMap = {
-  "@payloadcms/ui/rsc#FolderTableCell": FolderTableCell_ab83ff7e88da8d3530831f296ec4756a,
-  "@payloadcms/ui/rsc#FolderField": FolderField_ab83ff7e88da8d3530831f296ec4756a,
-  "payload-media-gallery/client#MediaListView": MediaListView_642efd537a03a6c81446512d5792dfd8,
-  "@payloadcms/ui#FolderTypeField": FolderTypeField_3817bf644402e67bfe6577f60ef982de,
+  "@sitebytom/payload-media-gallery/client#MediaListView": MediaListView_6f514b404e7f1589cac282e4c156f993,
   "@payloadcms/ui/rsc#CollectionCards": CollectionCards_ab83ff7e88da8d3530831f296ec4756a
 }
